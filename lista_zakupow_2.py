05cf1b8599b4f222f8dd8222a7ff9fa9c32a1a0a
Michal Supel <michalsupel@gmail.com>
2021-06-24 21:23:52 +0200

    lista_with_branch

--- lista_zakupow_2.py
+++ lista_zakupow_2.py
@@ -1,18 +1,1 @@
-print("Zadanie 1")
-Lista_zakupow = {
-    "piekarnia": ["chleb", "bułki", "pączek"],
-    "warzywniak": ["marchew", "seler", "rukola"]
-}
-
-
-ile = 0
-for key in Lista_zakupow:
-  lista = [a.capitalize() for a in Lista_zakupow[key]]
-  print("Idę do", key.capitalize(), "i kupuję tam", lista)
-  ile = ile + len(Lista_zakupow[key])
-print("W sumie kupuję", ile , "produktów")
-
-print("Zadanie 2")
-for number in range(0,101):
-    if number % 5 == 0:
-        print(number**3)
+print("Pozdrowienia dla mentora :)")
